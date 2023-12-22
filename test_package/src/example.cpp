cb0cd4f3c8b41dd5ff31ddcd96ff223f9f26d5b6
Michael Sohl <micso796@live.com>
2023-12-22 10:59:16 +0100

    add test_package and .gitignore

---
+++ test_package/src/example.cpp
@@ -0,0 +1,12 @@
+#include "car.h"
+#include <vector>
+#include <string>
+
+int main() {
+    car();
+
+    std::vector<std::string> vec;
+    vec.push_back("test_package");
+
+    car_print_vector(vec);
+}
